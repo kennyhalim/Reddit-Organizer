650914313601aa6c176fd84b231dc70807161057
Kenny Halim <kennyhalim98@gmail.com>
2019-04-05 09:24:49 -0700

    add model for userinfo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { UserInfo } from '../model/userInfo.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ src/model/userInfo.model.ts
@@ -0,0 +1,3 @@
+export class UserInfo {
+  constructor(public userName: string, public subreddit: string[], public userId: number) {}
+}
