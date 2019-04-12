fb8eeb5f278b05376768b8f1652a27cf02266548
Kenny Halim <kennyhalim98@gmail.com>
2019-04-12 12:11:38 -0700

    finish page for 1 subreddit

--- src/app/reddevils/reddevils.component.ts
+++ src/app/reddevils/reddevils.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ReddevilsService } from '../reddevils.service';
+import { UserinfoService } from '../userinfo.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-reddevils',
   templateUrl: './reddevils.component.html',
   styleUrls: ['./reddevils.component.css'],
-  providers: [ReddevilsService]
+  providers: [ReddevilsService, UserinfoService]
 })
 export class ReddevilsComponent implements OnInit {
   posts: any[] = null;
-  constructor(private reddevilsService: ReddevilsService) { }
+  users: FirebaseListObservable<any[]>;
+  testUser;
+  objectKeys = Object.keys;
+  constructor(private reddevilsService: ReddevilsService, private userInfoService: UserinfoService) { }
 
   ngOnInit() {
     this.getPost();
+    this.users = this.userInfoService.getUser();
+    this.testUser = this.userInfoService.getFirstUser();
   }
 
   getPost() {
