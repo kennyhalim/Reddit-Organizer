932fb23c2f1883ef298c3444d8ce22f6849b537a
Kenny Halim <kennyhalim98@gmail.com>
2019-04-12 11:27:53 -0700

    add method to get api call

--- src/app/reddevils/reddevils.component.ts
+++ src/app/reddevils/reddevils.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ReddevilsService } from '../reddevils.service';
 
 @Component({
   selector: 'app-reddevils',
   templateUrl: './reddevils.component.html',
-  styleUrls: ['./reddevils.component.css']
+  styleUrls: ['./reddevils.component.css'],
+  providers: [ReddevilsService]
 })
-export class ReddevilsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ReddevilsComponent {
+  posts: any[] = null;
+  constructor(private reddevilsService: ReddevilsService) { }
+  getData() {
+    this.reddevilsService.getPost().subscribe(response => {
+      this.posts = response.json();
+    });
   }
 
 }
